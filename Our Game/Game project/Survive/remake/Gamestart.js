448eb357affbf07a58af2734378aeb22ae4b0449
EmiiLiHe <32346135+Russ060826@users.noreply.github.com>
2017-12-06 16:00:08 -0800

    New changes

--- Our Game/Game project/Survive/remake/Gamestart.js
+++ Our Game/Game project/Survive/remake/Gamestart.js
@@ -12,16 +12,6 @@ function popupfunction(){
     document.getElementById('popup1btn3').style.display="none";
 }
 
-function diefunction(){
-    document.getElementById('popup1btn1').style.display="none";
-    document.getElementById('popup1btn2').style.display="none";
-    document.getElementById('startbtn').style.display="block";
-    document.getElementById('loadbtn').style.display="block";
-    document.getElementById('giveupbtn').style.display="block";
-    document.getElementById('popupbox').style.display="none";
-    document.getElementById('popup1btn3').style.display="none";
-}
-
 function youcantfunction(){
     document.getElementById('ays').innerHTML="Sorry, I haven't develop this function yet.";
     document.getElementById('ays').style.display="block";
@@ -46,9 +36,37 @@ function dienofunction(){
     document.getElementById('popup1btn3').style.display="block";
     document.getElementById('popup1btn2').style.display="block";
     document.getElementById('popup1btn2').innerHTML="......";
-        document.getElementById('popup1btn3').innerHTML="......";
+    document.getElementById('popup1btn3').innerHTML="......";
+}
+
+function diefunction(){
+    document.getElementById('popup1btn1').style.display="none";
+    document.getElementById('popup1btn2').style.display="none";
+    document.getElementById('startbtn').style.display="block";
+    document.getElementById('loadbtn').style.display="block";
+    document.getElementById('giveupbtn').style.display="block";
+    document.getElementById('popupbox').style.display="none";
+    document.getElementById('popup1btn3').style.display="none";
 }
 
 function hpopupfunction(){
-    
+    document.getElementById('popupbox').style.display="none";
+    document.getElementById('startscrn').style.display="none";
+    document.getElementById('storyscrn').style.display="block";
+    document.getElementById('storyscrn').style.backgroundColor="black";
+    thestoryfunction();
+}
+
+function thestoryfunction(){
+    document.getElementById('thestory').innerHTML="Sorry...The creater is still developing this game...>>>";
+}
+
+function changetextfunction(){
+    document.getElementById('thestory').innerHTML="Still, thanks for your playing...Maybe you can look for some pictures that I've draw for this game."
+}
+
+function skipfunction(){
+    document.getElementById('storyscrn').style.display="none";
+    document.getElementById('dramascreen').style.display="block";
+    document.getElementById('storyscrn').style.
 }
